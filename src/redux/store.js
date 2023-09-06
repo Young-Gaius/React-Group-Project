24b4b07e6319feb7f739499df6e26e6fcc8ff247
Armynerh <aimenah@outlook.com>
2023-09-06 12:23:15 +0100

    Modify store to pass linters

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import rocketsSlice from './rockets/rocketsSlice';
 
 const store = configureStore({
-  reducer: {
-    rockets: rocketsSlice,
-  },
+  reducer: {},
 });
 export default store;
