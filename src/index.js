3abfdf76e653bc2b4cc2872e1a75c1bca71e76f7
Armynerh <aimenah@outlook.com>
2023-09-06 12:07:40 +0100

    Modify Index file to include provider and store

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import { Provider } from 'react-redux';
+import './index.css';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </Router>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
