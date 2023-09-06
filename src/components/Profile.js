ccfecc78c4b826bd77896e92023091e638b64de1
Young-Gaius <youngehu8@gmail.com>
2023-09-06 16:07:57 +0000

    Display joined missions in My profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,27 @@
-const Profile = () => (
+import React from 'react';
+import { useSelector } from 'react-redux';
 
-  <h1>My profile</h1>
+function Profile() {
+  const { missions, joinedMissions } = useSelector((store) => store.missions);
+  const reservedMissions = missions.filter((mission) => joinedMissions.includes(mission.id));
+  return (
+    <div className="profile">
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">My Missions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {reservedMissions.map((mission) => (
+            <tr key={mission.id}>
+              <td>{mission.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
 
-);
 export default Profile;
